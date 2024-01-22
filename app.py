73afaf142f3107863f35571ed48c31fbf60f131e
Lautaro Figueroa <LautyfigueroaLau@gmail.com>
2024-01-22 18:58:05 -0300

    Puerto 5000

--- app.py
+++ app.py
@@ -166,4 +166,5 @@ def participants():
     }
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
